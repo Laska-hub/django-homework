19fb10d1d36d5e3c5e2c3a1c987cce195db03629
Olesia Laskovets <olesya_izvarina@mail.ru>
2026-05-05 21:28:22 +0300

    Add contacts model, custom command, latest products, fixtures fix

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from catalog.models import Product, Category, Contact
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "description")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "category", "created_at")
+    list_filter = ("category",)
+    search_fields = ("name",)
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ("id", "country", "inn", "address")
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from catalog.models import Product, Contact
+
 
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.order_by("-created_at")[:5]
+
+    print(products)  # вывод в консоль (по заданию)
+
+    return render(request, "home.html", {"products": products})
+
 
 def contacts(request):
-    return render(request, 'contacts.html')
+    contact = Contact.objects.first()
+
+    return render(request, "contacts.html", {"contact": contact})
