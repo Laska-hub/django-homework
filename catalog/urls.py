05d5165f03a975f8b22d156838d745992a07155b
Olesia Laskovets <olesya_izvarina@mail.ru>
2026-05-17 19:52:20 +0300

    Add CRUD for Product model

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
-from .views import HomeView, ContactsView, ProductDetailView
+from .views import (
+    ProductListView,
+    ProductDetailView,
+    ProductCreateView,
+    ProductUpdateView,
+    ProductDeleteView,
+)
 
 app_name = "catalog"
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('contacts/', ContactsView.as_view(), name='contacts'),
-    path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path("", ProductListView.as_view(), name="product_list"),
+    path("product/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+    path("product/create/", ProductCreateView.as_view(), name="product_create"),
+    path("product/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"),
+    path("product/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"),
 ]
 
